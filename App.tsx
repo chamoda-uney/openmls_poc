e02c720fc92366339b61a25b9a8fedf8dd4f6488
chamoda <chamoda@uney.com>
2024-07-18 16:45:57 +0400

    installed and configured firebase and firestore

--- App.tsx
+++ App.tsx
@@ -6,11 +6,9 @@
  */
 
 import React from 'react';
-import {Button, SafeAreaView, useColorScheme} from 'react-native';
+import {SafeAreaView, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-
-import {NativeModules} from 'react-native';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -19,47 +17,7 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const [registeredUserData, setRegisteredUserData] = React.useState();
-
-  const handleRegisterUser = () => {
-    const nativeF = NativeModules.OpenMLS.registerUser;
-    nativeF(
-      {
-        user_id: 'chamoda_abc',
-      },
-      (res: any) => {
-        setRegisteredUserData(res);
-      },
-    );
-  };
-
-  const handleInitMls = () => {
-    const nativeF = NativeModules.OpenMLS.initMls;
-    nativeF();
-  };
-
-  const handleCreateGroup = () => {
-    const nativeF = NativeModules.OpenMLS.createGroup;
-    nativeF(
-      {
-        group_id: 'group_abc',
-        registered_user_data: registeredUserData,
-      },
-      (res: any) => {
-        console.log(res);
-      },
-    );
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <Button title="0 - Init MLS" onPress={handleInitMls} />
-
-      <Button title="1 - Register User" onPress={handleRegisterUser} />
-
-      <Button title="2 - Create Group" onPress={handleCreateGroup} />
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={backgroundStyle} />;
 }
 
 export default App;
