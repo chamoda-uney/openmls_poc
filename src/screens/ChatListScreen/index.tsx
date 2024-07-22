0913f6e1784ac7dae85864fa96fe27780c80978b
chamoda <chamoda@uney.com>
2024-07-22 23:01:38 +0400

    implemented register user UI

--- src/screens/ChatListScreen/index.tsx
+++ src/screens/ChatListScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
-import {Appbar} from 'react-native-paper';
+import {FlatList, KeyboardAvoidingView, Platform, View} from 'react-native';
+import {Appbar, Button, TextInput} from 'react-native-paper';
 import {Group} from '../../sdk/storage-service/schema';
 import GroupListItem from '../../components/GroupListItem';
 
@@ -35,6 +35,41 @@ const ChatListScreen = () => {
 
 export default ChatListScreen;
 
+const RegisteredUserProfile = () => {
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+      }}>
+      <TextInput
+        mode="outlined"
+        label="Username"
+        placeholder="Username"
+        style={{
+          margin: 8,
+        }}
+      />
+      <TextInput
+        mode="outlined"
+        label="Display name"
+        placeholder="Display name"
+        style={{
+          margin: 8,
+        }}
+      />
+      <Button
+        style={{
+          margin: 8,
+        }}
+        mode="contained">
+        Register
+      </Button>
+    </KeyboardAvoidingView>
+  );
+};
 const ChatListScreenAppBar = () => {
   return (
     <Appbar.Header>
